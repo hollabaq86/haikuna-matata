66253efb093f07b99b17fcb676e7a865a26ebf18
Joan Petersohn <email": null>
2017-01-13 14:49:00 -0600

    got initial half of logic for probability hash working. still need to increment the value when duplicates are found

--- parser.py
+++ parser.py
@@ -1,16 +1,23 @@
 import re
 
-probabilityHash = {[], ""}
-#[word1, word2], count
+probabilityHash = {}
+#("word1 word2"), count
 
 def parseIntoProbabilityHash(text):
   stripPunctuation = re.sub(ur"[^\w\d'\s]+",' ',text)
   wordsInText = stripPunctuation.split()
-  n = 0
-  for word in wordsInText:
-    probabilityHash[wordsInText[n]] = 1
+  i = 0
+  count = len(wordsInText) - 1
+  while (i < count):
+    j = i + 1
+    word1 = wordsInText[i]
+    word2 = wordsInText[j]
+    print(word1 + " " + word2)
+    probabilityHash[word1 + " " + word2] = 1
+    i+=1
   return probabilityHash
 
+print(parseIntoProbabilityHash("how are you doing today, my friend?"))
 
 
 
