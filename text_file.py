177f168bde33a08e1d7ce65f317c75e2137a2896
hollabaq86 <hollis65@me.com>
2017-01-14 20:46:46 -0600

    clean out .pyc files to debug errors when running app, testing to see if we can persist data in db

--- text_file.py
+++ text_file.py
@@ -1,6 +1,5 @@
 import re
-from sqlalchemy.orm import sessionmaker
-Session = sessionmaker(bind=engine)
+from flask_sqlalchemy import SQLAlchemy
 
 haikuFile = open("haikus.txt")
 haikus = haikuFile.readlines()
@@ -8,10 +7,10 @@ haikus = haikuFile.readlines()
 probabilityHash = {}
 #("word1 word2"), count
 
-probabilityHash = {}
-
 def parseIntoProbabilityHash(text):
-  stripPunctuation = re.sub(ur"[^\w\d'\s]+",' ',text)
+  print(text)
+  for line in text:
+    stripPunctuation = re.sub(ur"[^\w\d'\s]+",' ',text)
   wordsInText = stripPunctuation.split()
   i = 0
   count = len(wordsInText) - 1
@@ -25,11 +24,10 @@ def parseIntoProbabilityHash(text):
     i+=1
   return probabilityHash
 
-
-print(parseIntoProbabilityHash("How are you doing today, my friend? Are you happy or are you sad? How are your kids doing today?"))
-
-for each_line in haikus:
-  parseIntoProbabilityHash(each_line)
-
-print(probabilityHash)
+def createUnigrams(hash):
+  for text, counter in hash:
+    split_text = text.split(" ")
+    new_unigram = Unigram(word1 = split_text[0], word2 = split_text[1], count = counter)
+    db.session.add(new_unigram)
+    db.session.commit()
 
