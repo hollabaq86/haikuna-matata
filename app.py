fb2fecd846fd5bebd1f8226838e33c8c772077c2
Dennis Marchetti <email": null>
2017-01-14 18:00:22 -0600

    adjust logic for migrations

--- app.py
+++ app.py
@@ -8,18 +8,29 @@ app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-from models import Result
+# from models import Unigrams
 
+
+# @app.route('/')
+# def hello():
+#     return "Hello World!"
+
+
+# @app.route('/<name>')
+# def hello_name(name):
+#     return "Hello {}!".format(name)
+
+
+# if __name__ == '__main__':
+#     app.run()
+
+from flask import render_template
 
 @app.route('/')
-def hello():
-    return "Hello World!"
+def index():
+    return 'Index Page'
 
-
-@app.route('/<name>')
-def hello_name(name):
-    return "Hello {}!".format(name)
-
-
-if __name__ == '__main__':
-    app.run()
+@app.route('/hello')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', name=name)
