d68c2e2fe48df037f557e4cc1568e1ec338a32dd
Noah Guy <email": null>
2017-01-16 12:25:02 -0600

    refine generateLine method

--- models.py
+++ models.py
@@ -1,5 +1,7 @@
+from app import db
 class Unigram(db.Model):
 	__tablename__ = "unigrams"
+	__table_args__ = {'extend_existing': True}
 
 	id = db.Column(db.Integer, primary_key=True)
 	word1 = db.Column('word1', db.String, nullable=False, index=True)
