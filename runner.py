c45c9a68ea2bc7937b4803cc6f33cf491d77da2b
hollabaq86 <hollis65@me.com>
2017-01-17 16:48:17 -0600

    fix looping error

--- runner.py
+++ runner.py
@@ -11,98 +11,81 @@ from random import randrange
 d = cmudict.dict()
 
 def numSylsInWord(word):
-  if word.lower() in d:
-    return [len(list(y for y in x if y[-1].isdigit())) for x in d[word.lower()]][0]
+	if word.lower() in d:
+		return [len(list(y for y in x if y[-1].isdigit())) for x in d[word.lower()]][0]
 
 def isHaiku(potentialHaiku):
-  syllableCount = countSyllables(potentialHaiku)
-  if syllableCount == 17:
-    #how do we want to break this down?
-    #what are the other "ifs" that this conditional needs to meet?
-    result = True
-  else:
-    result = False
+	syllableCount = countSyllables(potentialHaiku)
+	if syllableCount == 17:
+		#how do we want to break this down?
+		#what are the other "ifs" that this conditional needs to meet?
+		result = True
+	else:
+		result = False
 
-  return result
+	return result
 
 def countSyllables(potentialHaiku):
-  stripPunctuation = re.sub(ur"[^\w\d'\s]+",' ',potentialHaiku)
-  wordsInHaiku = stripPunctuation.split()
-  syllableCount = 0
-  for i in wordsInHaiku:
-    syllableCount += numSylsInWord(i)
-  return syllableCount
+	stripPunctuation = re.sub(ur"[^\w\d'\s]+",' ',potentialHaiku)
+	wordsInHaiku = stripPunctuation.split()
+	syllableCount = 0
+	for i in wordsInHaiku:
+		syllableCount += numSylsInWord(i)
+	return syllableCount
 
 def generateHaiku(firstWord):
-  haiku = ""
-  haiku += generateRandomLine(5, firstWord)
-  haiku += "\n"
-  haiku += generateRandomLine(7)
-  haiku += "\n"
-  haiku += generateRandomLine(5)
-  return haiku
+	haiku = ""
+	haiku += generateRandomLine(5, firstWord)
+	haiku += "\n"
+	haiku += generateRandomLine(7)
+	haiku += "\n"
+	haiku += generateRandomLine(5)
+	return haiku
 
 def generateRandomLine(sylCount, startingWord= None):
-  if not startingWord:
-    startingWord = pickRandomWord(sylCount)
-  remainingSylCount = sylCount - countSyllables(startingWord)
-  line = buildLine(remainingSylCount, [startingWord])
-  return " ".join(line)
+	if not startingWord:
+		startingWord = pickRandomWord(sylCount)
+	remainingSylCount = sylCount - countSyllables(startingWord)
+	line = buildLineList(remainingSylCount, [startingWord])
+	return " ".join(line)
 
 def buildLineList(sylCount, wordsFromBefore):
-  if sylCount == 0:
-    return wordsFromBefore
-  lastWord = wordsFromBefore[-1]
-  possibilities = grabPossibleWords(lastWord, sylCount) 
-  for possibileWord in possibilities:
-    newWordsFromBefore = [word[:] for word in wordsFromBefore]
-    newWordsFromBefore.append(possibileWord) 
-    newSyllableCount = sylCount - countSyllables(possibleWord)
-    result = buildLineList(newSyllableCount, newWordsFromBefore)
-    if not result:
-      return result
-  return None  
-
-  # if sylCount == 0:
-  #   return wordsFromBefore
-  # elif sylCount < 0:
-  #   return "You fucked up"
-  # if wordsFromBefore == None:
-  #   wordsFromBefore = pickRandomWord(sylCount)
-  # lastWord = wordsFromBefore.rsplit(None, 1)[-1]
-  # possibleWords = grabPossibleWords(lastWord, sylCount)
-  # if possibleWords:
-  #   index = randrange(0, len(possibleWords))
-  #   adder = possibleWords[index]
-  # if not possibleWords:
-  #   adder = pickRandomWord(sylCount)
-  # newwordsFromBefore = wordsFromBefore + " " + adder
-  # workingSylCount = sylCount - countSyllables(adder)
-  # return generateLine(workingSylCount, newBase)
+	if sylCount == 0:
+		return wordsFromBefore
+	lastWord = wordsFromBefore[-1]
+	possibilities = grabPossibleWords(lastWord, sylCount) 
+	for possibleWord in possibilities:
+		newWordsFromBefore = [word[:] for word in wordsFromBefore]
+		newWordsFromBefore.append(possibleWord) 
+		newSyllableCount = sylCount - countSyllables(possibleWord)
+		result = buildLineList(newSyllableCount, newWordsFromBefore)
+		if result:
+			return result
+	return None  
 
 def pickRandomWord(reqSylCount):
-  from models import Unigram
-  lengthDB = Unigram.query.count()
-  while True:
-    randomNumPick = randrange(0, lengthDB)
-    tryWord = Unigram.query.filter(Unigram.id == randomNumPick).first()
-    if countSyllables(tryWord.word1) <= reqSylCount:
-      word = tryWord.word1
-      break
-  return word  
+	from models import Unigram
+	lengthDB = Unigram.query.count()
+	while True:
+		randomNumPick = randrange(0, lengthDB)
+		tryWord = Unigram.query.filter(Unigram.id == randomNumPick).first()
+		if countSyllables(tryWord.word1) <= reqSylCount:
+			word = tryWord.word1
+			break
+	return word  
 
 def formatPossibleWords(unigrams, reqSylCount):
-  container = []
-  for each in unigrams:
-      for unigram in range(each.count):
-          if countSyllables(each.word2) <= reqSylCount:
-            container.append(each.word2)
-  return container
+	container = []
+	for each in unigrams:
+			for unigram in range(each.count):
+					if countSyllables(each.word2) <= reqSylCount:
+						container.append(each.word2)
+	return container
 
 def grabPossibleWords(baseWord, reqSylCount):
-  from models import Unigram
-  listOfUnigrams = Unigram.query.filter(Unigram.word1 ==baseWord)
-  return formatPossibleWords(listOfUnigrams, reqSylCount)     
+	from models import Unigram
+	listOfUnigrams = Unigram.query.filter(Unigram.word1 ==baseWord)
+	return formatPossibleWords(listOfUnigrams, reqSylCount)     
 
 # def identifyPartsOfSpeech(string):
 #   cleanString = re.sub(ur"[^\w\d'\s]+",' ', string)
