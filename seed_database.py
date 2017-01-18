df3ab056795c50a0ba58341aa1a98abace834ba9
hollabaq86 <hollis65@me.com>
2017-01-18 14:48:35 -0600

    parses all file into a hash, but there are formatting errors. Investigating

--- seed_database.py
+++ seed_database.py
@@ -16,24 +16,29 @@ def scrubText(text, punctuation):
   separatedLines = filter(None, separatedLines)
   return separatedLines
 
-def parseIntoProbabilityHash(text):
-  stripPunctuation = ""
+def parseIntoProbabilityHash(text, existingHash):
   stripPunctuationFailsafe = [re.sub(ur"[^\w\d'\s]+",'',string) for string in text]
   sectionsToParse = [string.split() for string in text]
-  print sectionsToParse
-  # # wordsInText = [stripPunctuation.split()]
-  # i = 0
-  # count = len(wordsInText) - 1
-  # while (i < count):
-  #   word1 = wordsInText[i].lower()
-  #   word2 = wordsInText[i+1].lower()
-  #   if word1 in d and word2 in d:
-  #     if (word1 + " " + word2) in probabilityHash:
-  #       probabilityHash[word1 + " " + word2] += 1
-  #     else:
-  #       probabilityHash[word1 + " " + word2] = 1
-  #   i+=1
-  # return probabilityHash
+  masterHash = existingHash
+  masterHash = [build_hash(masterHash, section) for section in sectionsToParse][0]
+  return masterHash
+
+
+def build_hash(existingHash, listToFormat):
+  i = 0
+  count = len(listToFormat) - 1
+  while (i < count):
+    twoWordString = listToFormat[i].lower() + " " + listToFormat[i+1].lower()
+    format_hash(existingHash, twoWordString)
+    i += 1
+  return existingHash
+
+def format_hash(existingHash, twoWordString):
+  if twoWordString in existingHash:
+    existingHash[twoWordString] += 1
+  else:
+    existingHash[twoWordString] = 1
+  return existingHash        
 
 def createUnigram(unigramSourcePair, count):
   split_text = unigramSourcePair.split(" ")
@@ -43,11 +48,12 @@ def createUnigram(unigramSourcePair, count):
 
 
 #runner logic
-# files = ['example_poetry/sample1.txt', 'example_poetry/sample2.txt', 'example_poetry/sample3.txt', 'example_poetry/sample4.txt', 'example_poetry/sample5.txt', 'example_poetry/sample6.txt']
-files = ["example_poetry/fix_parser.txt"]
+files = ['example_poetry/sample1.txt', 'example_poetry/sample2.txt', 'example_poetry/sample3.txt', 'example_poetry/sample4.txt', 'example_poetry/sample5.txt', 'example_poetry/sample6.txt']
+# files = ["example_poetry/fix_parser.txt", "example_poetry/fix_parser.txt"]
 testFiles = ['example_poetry/test_text.txt']
 # files variable passed in must be an array.  If only passing in one file, still must be an array.
 def seedDatabase(files):
+  hashed_haikus = {}  
   for txtfile in files:
     haikuFile = open(txtfile)
     haikus = haikuFile.readlines()
@@ -55,9 +61,10 @@ def seedDatabase(files):
     punctuationList = [".", "?", "!", ":", ";", "(", ")"]
     for punctuation in punctuationList:
       haikus = scrubText(haikus, punctuation)
-    hashed_haikus = parseIntoProbabilityHash(haikus)  
-
-  #   print("processing")
+    hashed_haikus = parseIntoProbabilityHash(haikus, hashed_haikus)  
+    print "****processing... current hash****"
+    print hashed_haikus
+    print "***********************************"
   # for sourcePair, count in hashed_haikus.items():
   #   createUnigram(sourcePair, count)
 
