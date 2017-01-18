84d3173549a893fb8f290aaa74909ac05a8facf6
Noah Guy <noahriguy@gmail.com>
2017-01-18 09:32:39 -0600

    Training (#53)
    * write tests, they pass
    
    * remove pyc files
    
    * first attempt at training in flask
    
    * fix for merge
    
    * fix for merge
    
    * fix route
    
    * fix for merge
    
    * fix for merge
    
    * add more text
    
    * remove pyc for merge

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import request
 from flask_sqlalchemy import SQLAlchemy
 import os
 # import run_text_files
-	
+
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
@@ -22,11 +22,29 @@ def hello():
 
 @app.route('/haiku', methods=['POST'])
 def haiku():
-  word = request.form['word']
-  processed_word = word.lower()
-  from runner import generateHaiku
-  result = generateHaiku(processed_word).split('\n')
-  return render_template('show.html', word=processed_word, result=result)
+    word = request.form['word']
+    processed_word = word.lower()
+    from runner import generateHaiku
+    result = generateHaiku(processed_word).split('\n')
+    return render_template('show.html', word=processed_word, result=result)
+
+@app.route('/train', methods=['POST'])
+def train():
+  update = request.form['update']
+  from training import favorUnigram, unfavorUnigram, favorLine, unfavorLine
+  if update == "like1":
+    favorLine(request.form['line1'])
+  elif update == "like2":
+    favorLine(request.form['line2'])
+  elif update == "like3":
+    favorLine(request.form['line3'])
+  elif update == "dislike1":
+    unfavorLine(request.form['line1'])
+  elif update == "dislike2":
+    unfavorLine(request.form['line2'])
+  else:
+    unfavorLine(request.form['line2'])
+  return render_template('hello.html')
 
 if __name__ == '__main__':
   app.run()
