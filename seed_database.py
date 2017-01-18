e8af4652c1493b8ebd448cd264ecefd6c59a4ab3
Dennis Marchetti <email": null>
2017-01-18 14:23:55 -0600

    change file name

--- seed_database.py
+++ seed_database.py
@@ -38,7 +38,7 @@ def createUnigram(unigramSourcePair, count):
 
 
 #runner logic
-files = ['example_poetry/sample1.txt', 'example_poetry/sample2.txt', 'example_poetry/sample3.txt', 'example_poetry/sample4.txt', 'example_poetry/sample5.txt', 'example_poetry/sample6.txt', 'example_poetry/sample7.txt', 'example_poetry/adelle.txt']
+files = ['example_poetry/sample1.txt', 'example_poetry/sample2.txt', 'example_poetry/sample3.txt', 'example_poetry/sample4.txt', 'example_poetry/sample5.txt', 'example_poetry/sample6.txt', 'example_poetry/sample7.txt', 'example_poetry/sample8.txt']
 testFiles = ['example_poetry/test_text.txt']
 # files variable passed in must be an array.  If only passing in one file, still must be an array.
 herokuTest = ['example_poetry/sample2.txt']
